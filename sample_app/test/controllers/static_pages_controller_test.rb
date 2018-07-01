0794de2d22cb206a87ffdb5bd5c27f26c6e39c8c
Kento75 <kento2github@gmail.com>
2018-07-01 18:29:24 +0900

    7月1日の学習 4章まで終わり

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -2,14 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should root" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
