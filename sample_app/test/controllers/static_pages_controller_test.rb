6cbf08c8dfe5e29a936edcdce7df4fb8a37a955e
Kento75 <kento2github@gmail.com>
2018-07-02 21:31:15 +0900

    7月2日の学習5-3-1まで

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -17,4 +17,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
