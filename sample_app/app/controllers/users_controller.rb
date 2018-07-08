cf84fb7b23d1ba5b6f4e3ed6a6c4b2534e55db48
Kento75 <kento2github@gmail.com>
2018-07-08 16:07:02 +0900

    7月8日の学習 7.28まで

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
