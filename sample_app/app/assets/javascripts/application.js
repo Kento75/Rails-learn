409f7e45b1fcb5862382b6656da7d327f51463f5
Kento75 <kento2github@gmail.com>
2018-07-16 21:14:49 +0900

    7月16日の学習 リスト 8.20まで終了

--- sample_app/app/assets/javascripts/application.js
+++ sample_app/app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery-ui/widgets/datepicker
 //= require rails-ujs
-//= require activestorage
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,8 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
