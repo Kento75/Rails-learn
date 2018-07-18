2ac800e21d974690015980848fe454289b5414aa
Kento75 <kento2github@gmail.com>
2018-07-18 20:53:54 +0900

    7月18日の学習 9.13終了

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
+      remember user
       redirect_to user
     else
       # エラーメッセージを作成する
